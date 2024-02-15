9b78af39866fd935479d52fe4f71ba9f2600d156
JonathanMPeres <emailmarceloperes@gmail.com>
2024-02-15 17:53:59 -0300

    function operation

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ import chalk from 'chalk';
 // internal modules
 import fs from 'fs';
 
-console.log('Starting The NJS Account!')
+operation();
+
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que voce deseja fazer?',
+        choices: [
+            'Criar Conta',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    }])
+    .then((answer) => {
+        const action = answer['action'];
+        console.log(action);
+    })
+    .catch((err) => console.log(err));
+}
