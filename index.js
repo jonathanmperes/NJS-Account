6108d01f338af805a4a62875a108b1f790c068c5
JonathanMPeres <emailmarceloperes@gmail.com>
2024-02-15 23:18:03 -0300

    function createAccount

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ function operation() {
     })
     .catch((err) => console.log(err));
 }
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir.'));
+}
