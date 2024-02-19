f117cdacaf2050cc11dd6da03f928fed5efc9fa5
JonathanMPeres <emailmarceloperes@gmail.com>
2024-02-19 13:21:23 -0300

    configuracao sair

--- index.js
+++ index.js
@@ -24,8 +24,17 @@ function operation() {
         const action = answer['action'];
         if(action === 'Criar Conta') {
             createAccount();
+        } else if(action === 'Depositar') {
+
+        } else if(action === 'Consultar Saldo') {
+
+        } else if(action === 'Sacar') {
+
+        } else if(action === 'Sair') {
+            console.log(chalk.bgBlue.black('Obrigado por usar o Accounts!'));
+            process.exit();
         }
-    })
+     })
     .catch((err) => console.log(err));
 }
 
