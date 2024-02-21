caef376c737661ecac90afc36f919bb9061c2b84
JonathanMPeres <emailmarceloperes@gmail.com>
2024-02-21 08:44:05 -0300

    metodo depositar

--- index.js
+++ index.js
@@ -87,6 +87,16 @@ function deposit() {
         if(!checkAccount(accountName)) {
             return deposit()
         }
+        inquirer.prompt([{
+            name: 'amount',
+            message: 'Quanto voce deseja depositar?'
+        }])
+        .then((answer) => {
+            const amount = answer['amount'];
+            addAmount(accountName, amount);
+            operation();
+        })
+        .catch((err) => console.log(err));
     })
     .catch(err => console.log(err));
 }
@@ -97,4 +107,29 @@ function checkAccount(accountName) {
         return false;
     }
     return true;
+}
+
+function addAmount(accountName, amount) {
+    const accountData = getAccount(accountName);
+    if (!amount) {
+        console.log(chalk.bgRed.black("Ocorreu um erro, tente novamente mais tarde!"));
+        return deposit();
+    }
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance);
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function (err) {
+            console.log(err);
+        }
+    )
+    console.log(chalk.green(`Foi depositado o valor de $${amount} na sua conta!`))
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf8',
+        flag: 'r'
+    })
+    return JSON.parse(accountJSON);
 }
